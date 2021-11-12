2b8ef610e1ddd524a74d084da7e965af7c0902c7
Brian Snipes <brian@snipes.org>
2021-11-12 08:45:14 -0600

    Updated to allow for multiple videos in zip file

--- overlay_victor_video.py
+++ overlay_victor_video.py
@@ -8,6 +8,8 @@ import sys
 import xmltodict
 from zipfile import ZipFile
 
+video_filenames = []
+
 
 def convert_integer_to_datetime(dt):
     dt = datetime.datetime.fromtimestamp(dt / 1000.0)
@@ -26,34 +28,46 @@ def get_manifest_from_zip(zf, manifest_filename):
         return zf_content.read(manifest_filename)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     if sys.argv[1]:
         # get zip contents
-        with ZipFile(sys.argv[1],'r') as zipfile:
+        print('Working on ' + sys.argv[1] + '\n')
+        with ZipFile(sys.argv[1], 'r') as zipfile:
             zipfile_filelist = zipfile.namelist()
         for item in zipfile_filelist:
             if '.mp4' in item:
-                video_filename = item
+                print('... MP4 found as ' + item + '\n')
+                video_filenames.append(item)
             if 'manifest.xml' in item:
+                print('... manifest.xml found as ' + item + '\n')
                 fh_contents = xmltodict.parse(get_manifest_from_zip(sys.argv[1], item))
-    # extract video
-    if not os.path.isfile(video_filename):
-        with ZipFile(sys.argv[1]) as zipfile:
-            zipfile.extract(video_filename)
 
-    mp4basename = fh_contents['sessions']['session']['string'][0]
-    room = fh_contents['sessions']['string'][1]
-    video_time = convert_integer_to_datetime(int(fh_contents['sessions']['session']['integer'][0]))
-    epoch_time = convert_integer_to_epoch(int(fh_contents['sessions']['session']['integer'][0]))
-    new_filename = ' timestamped_' + mp4basename
-    print(sys.argv[1], mp4basename, video_filename, room, video_time, epoch_time)
-    cmd = 'ffmpeg -i "' + os.path.join(video_filename) + '" -vf "drawtext=fontfile=font.ttf:fontsize=32:fontcolor=white:box=1:' \
-                                          + 'boxcolor=black@0.4:x=(w-text_w)/2:y=h-th-10:text=\'%{pts\:gmtime\:' \
-                                            + str(epoch_time) + '\:' + room + '   %m\/%d\/%Y %I\\\\\\\\\:%M\\\\\\\\\:%S %p CST}\'\"' \
-                                            + new_filename
-    # print(cmd)
-    if os.path.exists(new_filename):
-        os.remove(new_filename)
-    os.system(cmd)
+        room = fh_contents['sessions']['string'][1]
 
+        # extract video
+        for video in video_filenames:
+            if not os.path.isfile(video):
+                print('... MP4 not found so extracting video from zip - ' + sys.argv[1] + '\n')
+                with ZipFile(sys.argv[1]) as zipfile:
+                    zipfile.extract(video)
+        for videoindex in range(0, len(video_filenames)):
+            if len(video_filenames) == 1:
+                mp4basename = fh_contents['sessions']['session']['string'][0]
+                video_time = convert_integer_to_datetime(int(fh_contents['sessions']['session']['integer'][0]))
+                epoch_time = convert_integer_to_epoch(int(fh_contents['sessions']['session']['integer'][0]))
+            else:
+                mp4basename = fh_contents['sessions']['session'][videoindex]['string'][0]
+                video_time = convert_integer_to_datetime(
+                    int(fh_contents['sessions']['session'][videoindex]['integer'][0]))
+                epoch_time = convert_integer_to_epoch(int(fh_contents['sessions']['session'][videoindex]['integer'][0]))
+            new_filename = ' timestamped_' + mp4basename
+            print(sys.argv[1], mp4basename, video_filenames[videoindex], room, video_time, epoch_time)
+            cmd = 'ffmpeg -nostdin -i "' + os.path.join(
+                video_filenames[videoindex]) + '" -vf "drawtext=fontfile=font.ttf:fontsize=40:fontcolor=white:box=1:' \
+                  + 'boxcolor=black@0.4:x=(w-text_w)/2:y=h-th-10:text=\'%{pts\:gmtime\:' \
+                  + str(epoch_time) + '\:' + room + '   %m\/%d\/%Y %I\\\\\\\\\:%M\\\\\\\\\:%S %p CST}\'\"' \
+                  + new_filename + ' 2> /dev/null'
+            # print(cmd)
+            if os.path.exists(new_filename):
+                os.remove(new_filename)
+            os.system(cmd)
